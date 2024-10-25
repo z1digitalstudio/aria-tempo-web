842dba36bc2fdbda89d1f90674fb78d1d75dcf55
María Simó <holasoymariasimo@gmail.com>
2024-10-25 13:27:27 +0200

    feat(music-profile): apply scale to circles

--- src/app/music-profile/page.tsx
+++ src/app/music-profile/page.tsx
@@ -5,7 +5,8 @@ import { Header } from '@/components/header';
 import { createArrayOfSize } from '@/utils/array';
 import { links } from '@/utils/links';
 import clsx from 'clsx';
-import { motion } from 'framer-motion';
+import { motion, MotionValue, useMotionValue } from 'framer-motion';
+import { icon, useIconTransform } from './useIconTransform';
 
 const grid: number[][] = createArrayOfSize(5, [0, 1, 2, 3, 4]);
 
@@ -14,7 +15,7 @@ export default function MusicProfile() {
     <div className="relative bg-black text-creme size-full h-svh flex flex-col">
       <Header />
       <main className="size-full flex-1 flex flex-col min-h-0">
-        <div className="relative flex-1">
+        <div className="relative flex-1 overflow-hidden">
           <Grid />
         </div>
         <footer className="border-t border-white border-opacity-20 py-6 px-4">
@@ -30,52 +31,78 @@ export default function MusicProfile() {
   );
 }
 
-export const icon = {
-  margin: 20,
-  size: 100,
-};
-
 function Grid() {
-  const gridWidth = (icon.size + icon.margin) * 5;
+  const gridWidth = icon.size * 5 + icon.margin * 4;
   const gridHeight = icon.size * 5;
+  const x = useMotionValue(-100);
+  const y = useMotionValue(0);
 
   return (
     <motion.div
-      className="absolute top-1/2 left-1/2"
       style={{
         width: gridWidth,
         height: gridHeight,
-        transform: 'translate(-50%, -50%)',
+        x,
+        y,
       }}
+      drag
+      dragSnapToOrigin
     >
       {grid.map((rows, rowIndex) =>
         rows.map((colIndex) => (
-          <Item key={`${rowIndex}-${colIndex}`} row={rowIndex} col={colIndex} />
+          <Item
+            key={`${rowIndex}-${colIndex}`}
+            row={rowIndex}
+            col={colIndex}
+            planeX={x}
+            planeY={y}
+          />
         )),
       )}
     </motion.div>
   );
 }
 
-function Item({ row, col }: { row: number; col: number }) {
+function Item({
+  row,
+  col,
+  planeX,
+  planeY,
+}: {
+  row: number;
+  col: number;
+  planeX: MotionValue<number>;
+  planeY: MotionValue<number>;
+}) {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const scale = useMotionValue(1);
+
   const xOffset =
     col * (icon.size + icon.margin) +
     (row % 2) * ((icon.size + icon.margin) / 2);
   const yOffset = row * icon.size;
 
+  useIconTransform({ x, y, scale, planeX, planeY, xOffset, yOffset });
+
   return (
-    <div
+    <motion.div
       style={{
         top: yOffset,
         left: xOffset,
         width: icon.size,
         height: icon.size,
+        x,
+        y,
+        scale,
       }}
       className={clsx(
         row === 2 && 'bg-white',
         col === 2 && row === 2 && 'bg-[red]',
-        'absolute bg-creme rounded-full',
+        'absolute bg-creme rounded-full flex items-center justify-center text-black',
       )}
-    />
+    >
+      {xOffset}
+    </motion.div>
   );
 }
