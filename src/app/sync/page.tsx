9103e6f7b500525681857a1faa9b4b1210557717
María Simó <holasoymariasimo@gmail.com>
2024-10-28 09:57:23 +0100

    feat(sync): add start video

--- src/app/sync/page.tsx
+++ src/app/sync/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import SpotifyIcon from '@/assets/icons/spotify.svg';
 import { links } from '@/utils/links';
 import { Button } from '@/components/cta/button';
@@ -32,42 +31,55 @@ export default function Sync() {
   }
 
   return (
-    <main className="relative">
-      <div className="relative flex items-start size-full h-svh bg-[length:170%] bg-[center_top_-7.5rem]  md:bg-contain bg-no-repeat bg-black md:bg-center bg-[url('/whotels/img/sync/bg.png')]">
-        <div className="aspect-square md:h-full md:mt-[-4rem] w-full flex items-center justify-center">
-          <Image
-            src="/whotels/img/sync/shape.png"
-            alt=""
-            width={190}
-            height={190}
-          />
+    <main className="h-svh">
+      <div className="flex flex-col items-start size-full min-h-0">
+        {/** Get video with complete circles and, if possible, transparent bg to accomodate to larger screens */}
+        <div className="max-lg:aspect-square w-full min-h-0 flex items-center justify-center bg-[#09090a] flex-1">
+          <video
+            controls={false}
+            autoPlay
+            muted
+            className="size-full object-cover max-w-2xl"
+          >
+            <source
+              src="/whotels/video/start-sync-experience.mp4"
+              type="video/mp4"
+            />
+            <track
+              src="/path/to/captions.vtt"
+              kind="subtitles"
+              srcLang="en"
+              label="English"
+            />
+            Your browser does not support the video tag.
+          </video>
         </div>
-      </div>
-      <section className="absolute w-full bottom-0 bg-creme mt-auto text-black pt-12 pb-4 px-4 border-b-white border-b border-opacity-20 text-xl/6 text-center">
-        <div className="mb-10 flex flex-col gap-6">
-          <h1 className="type-headline-4">Connect to your Spotify</h1>
-          <p className="text-balance mb-3 type-body-2 opacity-50">
-            Link your Spotify profile to get more personalized experience plus
-            your music profile.
-          </p>
-        </div>
+        <section className="w-full bg-creme mt-auto text-black items-center pt-10 pb-4 px-4 text-xl/6 text-center content-between flex flex-col">
+          <div className="flex flex-col gap-6 mb-6">
+            <h1 className="type-headline-4">Connect to your Spotify</h1>
+            <p className="text-balance mb-3 type-body-2 opacity-50">
+              Link your Spotify profile to get more personalized experience plus
+              your music profile.
+            </p>
+          </div>
 
-        <div className="flex gap-2 flex-col">
-          <Button
-            Icon={SpotifyIcon}
-            label="Connect to Spotify"
-            isFullWidth
-            onClick={handleSyncSpotify}
-          />
-          <Button
-            label="Connect later"
-            variant="secondary"
-            outline={false}
-            isFullWidth
-            onClick={handleSkipSyncing}
-          />
-        </div>
-      </section>
+          <div className="flex gap-2 flex-col mt-auto w-full">
+            <Button
+              Icon={SpotifyIcon}
+              label="Connect to Spotify"
+              isFullWidth
+              onClick={handleSyncSpotify}
+            />
+            <Button
+              label="Connect later"
+              variant="secondary"
+              outline={false}
+              isFullWidth
+              onClick={handleSkipSyncing}
+            />
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
